4e6ccddfbdf8b010f51fb229ccf1c49483b0b8b5
Reinier Hernández <sasuke.reinier@gmail.com>
2024-11-29 17:44:47 -0300

    fix(Formula): update SHA256 in getmyip.rb

--- Formula/getmyip.rb
+++ Formula/getmyip.rb
@@ -2,7 +2,7 @@ class Getmyip < Formula
   desc "Get your public IP Address"
   homepage "https://github.com/ragnarok22/getmyip"
   url "https://github.com/ragnarok22/getmyip/archive/v1.0.0.tar.gz"
-  sha256 "7d4d1fe8154c56c2635ef668ae50d329ea61c28ec9741340c7554a7b9ffedb89"
+  sha256 "4bd3faf3135ec3534e2531a0743a6abc01e4a68226d52671b4f84adf3a24420a"
   version "1.0.0"
 
   def install
